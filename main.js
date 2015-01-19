d9973636efa8c7aefd1ec57ab99464ec38d06549
mmilito <militomariel@gmail.com>
2015-01-19 16:06:12 -0700

    exercise complete

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+var word =prompt("type any word here:");
+alert ("Your word: " + word +"\n"+
+"Your word length: " + word.length +"\n"+
+"Letter in position 3: " + word[2] +"\n"+
+"Your word in lowercase: " + word.toLowerCase() +"\n"+
+"Your word in uppercase: " + word.toUpperCase() +"\n"+
+"Your word in a sentence: " +"The quick red fox jumped over the lazy brown "+word+ "." +"\n"+
+"Your subword, characters 2-4: "+ word[1]+word[2]+word[3]);
